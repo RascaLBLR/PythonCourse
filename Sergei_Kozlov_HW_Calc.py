78c2971a883e302e5eb26e896b22dc7121064b81
RascaLBLR <106335862+RascaLBLR@users.noreply.github.com>
2022-05-30 21:54:34 +0300

    Add files via upload

---
+++ Sergei_Kozlov_HW_Calc.py
@@ -0,0 +1,13 @@
+weight = int(input("Введите ваш вес(кг.):"))
+height = int(input("Введите ваш рост(см.):"))
+
+bmi = weight / (height / 100) ** 2
+
+print("Ваш индекс массы тела:" , round(bmi , 2) ,
+    "\n20" + "=" * (int(bmi) - 20) + "|" + "=" * 
+    (50 - int(bmi)) + "50")
+
+#l_equals = int(bmi - 20)
+#r_equals = int(50 - bmi)
+#print("Ваш индекс массы тела:" , round(bmi , 2) ,
+    #"\n20" + "=" * l_equals + "|" + "=" * r_equals + "50")
