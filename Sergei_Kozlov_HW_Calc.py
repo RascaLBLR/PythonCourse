4901075b502ae99f656f55a15ac4065db83b64a4
RascaLBLR <106335862+RascaLBLR@users.noreply.github.com>
2022-06-01 10:01:08 +0300

    Update Sergei_Kozlov_HW_Calc.py

--- Sergei_Kozlov_HW_Calc.py
+++ Sergei_Kozlov_HW_Calc.py
@@ -3,11 +3,16 @@ height = int(input("Введите ваш рост(см.):"))
 
 bmi = weight / (height / 100) ** 2
 
-print("Ваш индекс массы тела:" , round(bmi , 2) ,
+print(
+    "Ваш индекс массы тела:" , round(bmi , 2) ,
     "\n20" + "=" * (int(bmi) - 20) + "|" + "=" * 
-    (50 - int(bmi)) + "50")
+    (50 - int(bmi)) + "50"
+)
 
+#variables solution
 #l_equals = int(bmi - 20)
 #r_equals = int(50 - bmi)
-#print("Ваш индекс массы тела:" , round(bmi , 2) ,
-    #"\n20" + "=" * l_equals + "|" + "=" * r_equals + "50")
+#print(
+#"Ваш индекс массы тела:" , round(bmi , 2) ,
+    #"\n20" + "=" * l_equals + "|" + "=" * r_equals + "50"
+#)
